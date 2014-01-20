f6b93da7e654140c5f4de8b16d6fc794bf21f805
codyzazulak1 <codyzazulak1@gmail.com>
2014-01-20 23:09:17 +0000

    Pre-Lesson push

--- application.rb
+++ application.rb
@@ -2,13 +2,30 @@ class Application
  
   def initialize
     # Start with an empty array of contacts.
-    # TODO: Perhaps stub (seed) some contacts so we don't have to create some every time we restart the app
+    # TODO: Perhaps stub (seed) some contacts so we don't 
+    # have to create some every time we restart the app.
     @contacts = []
   end
  
   def run
     show_main_menu
-    input = gets.chomp
+    input = gets.chomp.downcase
+    case input
+      when "new"
+        new_contact
+        #complete
+      when "list"
+        list_all
+        #complete
+      when "show :id"
+        puts "K3"
+        #insert display here
+      when "quit"
+        puts "Goodbye!"
+      else 
+        puts "Wrong answer! Try again, Biatch!"
+        run 
+      end
   end
   
   # Prints the main menu only
@@ -17,7 +34,33 @@ class Application
     puts " new      - Create a new contact"
     puts " list     - List all contacts"
     puts " show :id - Display contact details"
+    puts " quit     - Quits the program"
     print "> "
   end
+
+  def new_contact
+    puts "Cool, can you tell me your full name?"
+    name = gets.chomp
+    puts "...and your email?"
+    email = gets.chomp
+    puts "Thanks, bro!"
+    contact = Contact.new(name, email)
+    @contacts.push(contact)
+    run
+    #complete
+  end
+
+  def list_all
+    @contacts.each do |contact|
+      puts contact.to_s
+    end
+    
+    run
+    #complete
+  end
+
+  def display
+  end
+
  
 end
